30fdf48f47f48665e11e3c8daccc248d4adc3a6e
Kotatumikann <tatsuyakonishi@kotatubotno-MacBook-Pro.local>
2017-11-09 23:48:23 +0900

    repaire get

--- Pscp/ssh_main.py
+++ Pscp/ssh_main.py
@@ -5,6 +5,17 @@ from scp_get import Scp_Get
 from scp_file_path import Copy_File_Path
 
 
+def get_dir(ssh,scp):
+    scp_get = Scp_Get(ssh,scp)
+    remote_path = '/home/pi/TEST'
+    data = scp_get.get_data(remote_path)
+    local_path = "/Users/tatsuyakonishi/Documents/Lok"
+    cfp = Copy_File_Path()
+    remote_copy_file,local_create_dir,local_file_path = cfp.get_path(remote_path,local_path,data)
+    scp_get.local_create_dir(local_create_dir)
+    scp_get.local_create_file(remote_copy_file,local_file_path)
+
+
 HOST = "192.168.10.77"
 PORT = 22
 USER = "pi"
@@ -14,17 +25,8 @@ cfp = Copy_File_Path()
 ssh.set_missing_host_key_policy(AutoAddPolicy())
 ssh.connect(hostname=HOST,port=PORT,username=USER,password=Password)
 scp = SCPClient(ssh.get_transport())
-scp_get = Scp_Get(ssh,scp)
-remote_path = '/home/pi/TEST'
-data = scp_get.get_data(remote_path)
-local_path = "/Users/tatsuyakonishi/Documents/Lok"
-cfp = Copy_File_Path()
-remote_copy_file,local_create_dir,local_file_path = cfp.get_path(remote_path,local_path,data)
-scp_get.local_create_dir(local_create_dir)
-scp_get.local_create_file(remote_copy_file,local_file_path)
-
-
-
+# getする
+get_dir(ssh,scp)
 
 scp.close()
 ssh.close()
