279eea0794e06f12a00b3f6a1b21da58876bc136
Kotatu <kotatubot@gmail.com>
2017-11-20 16:16:44 +0900

    install

--- Pscp/ssh_main.py
+++ Pscp/ssh_main.py
@@ -5,50 +5,54 @@ from getpass import getpass
 from ssh_help import SSh_Help
 
 
-message1 = "You can check arguments with the help"
-message  = """  
-              ________  ________  ________  ________
-             |\   __  \|\   ____\|\   ____\|\   __  \  
-             \ \  \|\  \ \  \___|\ \  \___|\ \  \|\  \ 
-              \ \   ____\ \_____  \ \  \    \ \   ____
-               \ \  \___|\|____|\  \ \  \____\ \  \___|
-                \ \__\     ____\_\  \ \_______\ \__\   
-                 \|__|    |\_________\|_______|\|__|   
-                          \|_________|                 
-          """                                                               
+def main():
+    message1 = "You can check arguments with the help"
+    message  = """  
+                  ________  ________  ________  ________
+                 |\   __  \|\   ____\|\   ____\|\   __  \  
+                 \ \  \|\  \ \  \___|\ \  \___|\ \  \|\  \ 
+                  \ \   ____\ \_____  \ \  \    \ \   ____
+                   \ \  \___|\|____|\  \ \  \____\ \  \___|
+                    \ \__\     ____\_\  \ \_______\ \__\   
+                     \|__|    |\_________\|_______|\|__|   
+                              \|_________|                 
+              """                                                               
 
-print(message1)
-print(message)
+    print(message1)
+    print(message)
 
-while True:
-    commands = input(">>> ")
+    while True:
+        commands = input(">>> ")
 
-    if commands=="exit":
-        sys.exit()
-    elif commands == "help":
-        shelp = SSh_Help()
-        shelp.help_show()
-    else:
-        command = Command()
-        # optionのディクトを作成する
-        dicts = command.command_start(commands)
-        sc = Select_Choice(dicts)
-        if dicts["action"]=="get" or dicts["action"]=="put":
-                from_string = dicts["from"]
-                to_string = dicts["to"]
-                if from_string == "from" or to_string == "to":
-                    print("Path is not set for --from or --to")
-                elif from_string == "" or to_string=="":
-                    print("There is too much space between --from or --to and the path.")
-                    print("Please leave one blank space")
-                else:
-                    print(froms,to)
-                    password = getpass("Password: ")
-                    # それぞれの処理を行う
-                    sc.get_put_fetch(password)
-        elif dicts["action"]=="show":
-            sc.shows()
-        elif dicts["action"]=="register":
-            sc.register()
+        if commands=="exit":
+            sys.exit()
+        elif commands == "help":
+            shelp = SSh_Help()
+            shelp.help_show()
         else:
-           print("Icorrect argument or command ")
+            command = Command()
+            # optionのディクトを作成する
+            dicts = command.command_start(commands)
+            sc = Select_Choice(dicts)
+            if dicts["action"]=="get" or dicts["action"]=="put":
+                    from_string = dicts["from"]
+                    to_string = dicts["to"]
+                    if from_string == "from" or to_string == "to":
+                        print("Path is not set for --from or --to")
+                    elif from_string == "" or to_string=="":
+                        print("There is too much space between --from or --to and the path.")
+                        print("Please leave one blank space")
+                    else:
+                        print(froms,to)
+                        password = getpass("Password: ")
+                        # それぞれの処理を行う
+                        sc.get_put_fetch(password)
+            elif dicts["action"]=="show":
+                sc.shows()
+            elif dicts["action"]=="register":
+                sc.register()
+            else:
+               print("Icorrect argument or command ")
+
+if ___name__=="__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+
+setup(
+        name="Pscp",
+        version="0.1",
+        decription="Application that makes scp command easy",
+        url="https://github.com/KotatuBot/Pscp.git",
+        pacakges=find_packages(),
+        entry_points="""
+                    [console_scripts]
+                    pscp = ssh_main.py:main
+                     """,
+        )
