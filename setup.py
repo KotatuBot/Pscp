5c2409eb409ef14e6b065bed2113872ed9a9d249
Kotatu <kotatubot@gmail.com>
2017-11-20 23:16:29 +0900

    repaire setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup,find_packages
 setup(
         name="Pscp",
         version="0.1",
-        decription="Application that makes scp command easy",
+        description="Application that makes scp command easy",
         url="https://github.com/KotatuBot/Pscp",
-        pacakges=find_packages(),
+        packages=find_packages(),
         entry_points={
             'console_scripts':['Pscp = Pscp.ssh_main:main']
             },
